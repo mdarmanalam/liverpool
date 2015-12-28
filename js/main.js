b383296d0e2dfcf5a49dbd7c1fa10cc18c1cf558
Arman Alam <arman.alam@Intelegencia.com>
2015-12-28 20:18:41 +0530

    Commit on 28/12/2015

--- js/main.js
+++ js/main.js
@@ -1,1 +1,25 @@
+/*Menu handler*/
 
+/* $(function() {
+    $('nav li a').click(function(e) {
+        e.preventDefault();
+        var $this = $(this);
+        $this.closest('ul').find('.active').removeClass('active');
+        $this.parent().addClass('active');
+
+    });
+});
+ */
+ 
+$(function(){
+	var url = window.location.pathname;  
+	var activePage = url.substring(url.lastIndexOf('/')+1);
+	$('.nav li a').each(function(){  
+	var currentPage = this.href.substring(this.href.lastIndexOf('/')+1);
+		if (activePage == currentPage) {
+		var $this = $(this);
+		$this.closest('ul').find('.active').removeClass('active');
+		$this.parent().addClass('active');		
+		} 
+	});
+})
